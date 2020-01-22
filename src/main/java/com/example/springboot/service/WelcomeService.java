5b20d6aa6ab5fc7dde45077c99f34c56bb657e55
unknown <izuekenonsov@gmail.com>
2020-01-22 18:52:15 +0100

    Added yaml configuration file

--- src/main/java/com/example/springboot/service/WelcomeService.java
+++ src/main/java/com/example/springboot/service/WelcomeService.java
@@ -1,10 +1,17 @@
 package com.example.springboot.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class WelcomeService {
+	
+	@Value("${welcome.message}")
+	private String welcomeMessage;
+	
+	
 	public String welcomeMessage() {
-		return "Good morning updated";
+//		return "Good morning updated";
+		return welcomeMessage;
 	}
 }
