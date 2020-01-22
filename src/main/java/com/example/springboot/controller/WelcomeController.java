96edef7a309279f11fcb535836fd4bc3ca376fa2
unknown <izuekenonsov@gmail.com>
2020-01-22 19:23:38 +0100

    Added dynamic configuration using configuration beans

--- src/main/java/com/example/springboot/controller/WelcomeController.java
+++ src/main/java/com/example/springboot/controller/WelcomeController.java
@@ -1,9 +1,14 @@
 package com.example.springboot.controller;
 
+
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.springboot.configuration.BasicConfiguration;
 import com.example.springboot.service.WelcomeService;
 
 @RestController
@@ -12,8 +17,23 @@ public class WelcomeController {
 	@Autowired
 	WelcomeService service;
 	
+	@Autowired
+	private BasicConfiguration configuration;
+	
 	@RequestMapping(value="/welcome")
 	public String showWelcomePage() {
 		return service.welcomeMessage();
+	}
+	
+	@RequestMapping("/dynamic-configuration")
+	public Map dynamicConfiguration() {
+		
+		Map map = new HashMap();
+		
+		map.put("message", configuration.getMessage());
+		map.put("number", configuration.getNumber());
+		map.put("value", configuration.isValue());
+		
+		return map;
 	}
 }
