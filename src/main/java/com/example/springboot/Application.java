ed096e7844c2aeab412d1b26bd1b74e197d34b1f
unknown <izuekenonsov@gmail.com>
2020-01-22 19:03:42 +0100

    worked on configuration using profiles

--- src/main/java/com/example/springboot/Application.java
+++ src/main/java/com/example/springboot/Application.java
@@ -3,7 +3,9 @@ package com.example.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 @ComponentScan("com.example")
@@ -12,6 +14,12 @@ public class Application {
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(Application.class, args);
 
+	}
+	
+	@Profile("prod")
+	@Bean
+	public String dummy() {
+		return "something";
 	}
 
 }
