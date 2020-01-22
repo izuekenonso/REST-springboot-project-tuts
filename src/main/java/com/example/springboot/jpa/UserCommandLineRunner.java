c8dde7bde6062549e32029b72b548ec0bb02ac93
unknown <izuekenonsov@gmail.com>
2020-01-22 20:28:28 +0100

    Added and retrieved User data from h2database using the CrudRepository

--- src/main/java/com/example/springboot/jpa/UserCommandLineRunner.java
+++ src/main/java/com/example/springboot/jpa/UserCommandLineRunner.java
@@ -1,15 +1,29 @@
 package com.example.springboot.jpa;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserCommandLineRunner implements CommandLineRunner{
+	
+	private static final Logger log = LoggerFactory.getLogger(UserCommandLineRunner.class);
 
+	@Autowired
+	private UserRepository repository;
+	
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Usercommandlinerunner");		
+		repository.save(new User("Nonso", "Admin"));
+		repository.save(new User("Dami", "User"));
+		repository.save(new User("Dimeji", "Admin"));
+		repository.save(new User("Onyinyechi", "User"));
+		
+		for (User user : repository.findAll()) {
+			log.info(user.toString());
+		}
 	}
-
 	
 }
